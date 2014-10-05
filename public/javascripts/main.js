51c2df66ab1821131d638a4f8bcb7f9f165f5fd7
Jeremy <jeremykentc@gmail.com>
2014-10-05 19:23:34 -0400

    updating thermometer and javascript

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,25 +1,56 @@
+var goal = 165000;
+
+var amounts = [
+    141420,
+    117850,
+    94280,
+    70710,
+    47140,
+    23570
+];
+
+
+
+
 $(document).ready(function() {
+    setupClickHandlers();
+    setAmounts();
+});
+
+function setupClickHandlers() {
     $("#thermometer").click(function(e) {
         changeLevel(e.pageY);
+        if (e.pageY < 5) {
+            setTimeout(function() {
+                $("#container").fadeOut(1000);
+                $("#body").fadeOut(1000);
+                $('#black').animate({
+                    opacity: 1
+                }, 1000, function () {
+                    launchFireworksWhenGoalIsMet();
+                    $('#canvas_text').fadeIn();
+                });
+            }, 2000);
+        }
     });
+}
 
-    var amounts = [165000, 141420, 117850, 94280, 70710, 47140, 23570];
+function setAmounts() {
+    $("#goal").text("$" + goal.format());
 
-    $("#goal").text("$" + amounts[0].format());
-    $("#sub-goal-left-1").text("$" + amounts[1].format());
-    $("#sub-goal-right-2").text("$" + amounts[2].format());
-    $("#sub-goal-left-3").text("$" + amounts[3].format());
-    $("#sub-goal-right-4").text("$" + amounts[4].format());
-    $("#sub-goal-left-5").text("$" + amounts[5].format());
-    $("#sub-goal-right-6").text("$" + amounts[6].format());
-    //call without the parameters to have it read from the DOM
-//    thermometer("thermo1");
-    // or with parameters if you want to update it using JavaScript.
-    // you can update it live, and choose whether to show the animation
-    // (which you might not if the updates are relatively small)
-//    thermometer("thermo2", 1000000, 425610 );
-
-});
+    if (amounts.length == 0) {
+        for (var i = 1; i < 6; i ++) {
+            var amount = (goal / 7) * i; // Round down to nearest whole ten digit
+            amounts.push(amount);
+        }
+    }
+    $("#sub-goal-left-1").text("$" + amounts[0].format());
+    $("#sub-goal-right-2").text("$" + amounts[1].format());
+    $("#sub-goal-left-3").text("$" + amounts[2].format());
+    $("#sub-goal-right-4").text("$" + amounts[3].format());
+    $("#sub-goal-left-5").text("$" + amounts[4].format());
+    $("#sub-goal-right-6").text("$" + amounts[5].format());
+}
 
 function changeLevel(y){
     var hide = $("#hide");
@@ -31,66 +62,258 @@ Number.prototype.format = function(n, x) {
     return this.toFixed(Math.max(0, ~~n)).replace(new RegExp(re, 'g'), '$&,');
 };
 
-//originally from http://stackoverflow.com/questions/149055/how-can-i-format-numbers-as-money-in-javascript
-function formatCurrency(n, c, d, t) {
-    "use strict";
+// fireworks - http://jsfiddle.net/dtrooper/AceJJ/
+var SCREEN_WIDTH = window.innerWidth,
+    SCREEN_HEIGHT = window.innerHeight,
+    mousePos = {
+        x: 400,
+        y: 300
+    },
 
-    var s, i, j;
+// create canvas
+    canvas = document.createElement('canvas'),
+    context = canvas.getContext('2d'),
+    particles = [],
+    rockets = [],
+    MAX_PARTICLES = 400,
+    colorCode = 0;
 
-    c = isNaN(c = Math.abs(c)) ? 2 : c;
-    d = d === undefined ? "." : d;
-    t = t === undefined ? "," : t;
-
-    s = n < 0 ? "-" : "";
-    i = parseInt(n = Math.abs(+n || 0).toFixed(c), 10) + "";
-    j = (j = i.length) > 3 ? j % 3 : 0;
-
-    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+// init
+function launchFireworksWhenGoalIsMet() {
+    document.body.appendChild(canvas);
+    canvas.width = SCREEN_WIDTH;
+    canvas.height = SCREEN_HEIGHT;
+    setInterval(launch, 800);
+    setInterval(loop, 1000 / 50);
 }
 
+// update mouse position
+$(document).mousemove(function(e) {
+    e.preventDefault();
+    mousePos = {
+        x: e.clientX,
+        y: e.clientY
+    };
+});
 
-/**
- * Thermometer Progress meter.
- * This function will update the progress element in the "thermometer"
- * to the updated percentage.
- * If no parameters are passed in it will read them from the DOM
- *
- * @param {Number} goalAmount The Goal amount, this represents the 100% mark
- * @param {Number} progressAmount The progress amount is the current amount
- * @param {Boolean} animate Whether to animate the height or not
- *
- */
-function thermometer(id, goalAmount, progressAmount, animate) {
-    "use strict";
-
-    var $thermo = $("#"+id),
-        $progress = $(".progress", $thermo),
-        $goal = $(".goal", $thermo),
-        percentageAmount,
-        isHorizontal = $thermo.hasClass("horizontal"),
-        newCSS = {};
-
-    goalAmount = goalAmount || parseFloat( $goal.text() ),
-        progressAmount = progressAmount || parseFloat( $progress.text() ),
-        percentageAmount =  Math.min( Math.round(progressAmount / goalAmount * 1000) / 10, 100); //make sure we have 1 decimal point
-
-    //let's format the numbers and put them back in the DOM
-    $goal.find(".amount").text( "$" + formatCurrency( goalAmount ) );
-    $progress.find(".amount").text( "$" + formatCurrency( progressAmount ) );
-
-
-    //let's set the progress indicator
-    $progress.find(".amount").hide();
-
-    newCSS[ isHorizontal ? "width" : "height" ] = percentageAmount + "%";
-
-    if (animate !== false) {
-        $progress.animate( newCSS, 1200, function(){
-            $(this).find(".amount").fadeIn(500);
-        });
+// launch more rockets!!!
+$(document).mousedown(function(e) {
+    for (var i = 0; i < 5; i++) {
+        launchFrom(Math.random() * SCREEN_WIDTH * 2 / 3 + SCREEN_WIDTH / 6);
     }
-    else {
-        $progress.css( newCSS );
-        $progress.find(".amount").fadeIn(500);
+});
+
+function launch() {
+    launchFrom(mousePos.x);
+}
+
+function launchFrom(x) {
+    if (rockets.length < 10) {
+        var rocket = new Rocket(x);
+        rocket.explosionColor = Math.floor(Math.random() * 360 / 10) * 10;
+        rocket.vel.y = Math.random() * -3 - 4;
+        rocket.vel.x = Math.random() * 6 - 3;
+        rocket.size = 8;
+        rocket.shrink = 0.999;
+        rocket.gravity = 0.01;
+        rockets.push(rocket);
     }
 }
+
+function loop() {
+    // update screen size
+    if (SCREEN_WIDTH != window.innerWidth) {
+        canvas.width = SCREEN_WIDTH = window.innerWidth;
+    }
+    if (SCREEN_HEIGHT != window.innerHeight) {
+        canvas.height = SCREEN_HEIGHT = window.innerHeight;
+    }
+
+    // clear canvas
+    context.fillStyle = "rgba(0, 0, 0, 0.05)";
+    context.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    var existingRockets = [];
+
+    for (var i = 0; i < rockets.length; i++) {
+        // update and render
+        rockets[i].update();
+        rockets[i].render(context);
+
+        // calculate distance with Pythagoras
+        var distance = Math.sqrt(Math.pow(mousePos.x - rockets[i].pos.x, 2) + Math.pow(mousePos.y - rockets[i].pos.y, 2));
+
+        // random chance of 1% if rockets is above the middle
+        var randomChance = rockets[i].pos.y < (SCREEN_HEIGHT * 2 / 3) ? (Math.random() * 100 <= 1) : false;
+
+        /* Explosion rules
+         - 80% of screen
+         - going down
+         - close to the mouse
+         - 1% chance of random explosion
+         */
+        if (rockets[i].pos.y < SCREEN_HEIGHT / 5 || rockets[i].vel.y >= 0 || distance < 50 || randomChance) {
+            rockets[i].explode();
+        } else {
+            existingRockets.push(rockets[i]);
+        }
+    }
+
+    rockets = existingRockets;
+
+    var existingParticles = [];
+
+    for (var i = 0; i < particles.length; i++) {
+        particles[i].update();
+
+        // render and save particles that can be rendered
+        if (particles[i].exists()) {
+            particles[i].render(context);
+            existingParticles.push(particles[i]);
+        }
+    }
+
+    // update array with existing particles - old particles should be garbage collected
+    particles = existingParticles;
+
+    while (particles.length > MAX_PARTICLES) {
+        particles.shift();
+    }
+}
+
+function Particle(pos) {
+    this.pos = {
+        x: pos ? pos.x : 0,
+        y: pos ? pos.y : 0
+    };
+    this.vel = {
+        x: 0,
+        y: 0
+    };
+    this.shrink = .97;
+    this.size = 2;
+
+    this.resistance = 1;
+    this.gravity = 0;
+
+    this.flick = false;
+
+    this.alpha = 1;
+    this.fade = 0;
+    this.color = 0;
+}
+
+Particle.prototype.update = function() {
+    // apply resistance
+    this.vel.x *= this.resistance;
+    this.vel.y *= this.resistance;
+
+    // gravity down
+    this.vel.y += this.gravity;
+
+    // update position based on speed
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+
+    // shrink
+    this.size *= this.shrink;
+
+    // fade out
+    this.alpha -= this.fade;
+};
+
+Particle.prototype.render = function(c) {
+    if (!this.exists()) {
+        return;
+    }
+
+    c.save();
+
+    c.globalCompositeOperation = 'lighter';
+
+    var x = this.pos.x,
+        y = this.pos.y,
+        r = this.size / 2;
+
+    var gradient = c.createRadialGradient(x, y, 0.1, x, y, r);
+    gradient.addColorStop(0.1, "rgba(255,255,255," + this.alpha + ")");
+    gradient.addColorStop(0.8, "hsla(" + this.color + ", 100%, 50%, " + this.alpha + ")");
+    gradient.addColorStop(1, "hsla(" + this.color + ", 100%, 50%, 0.1)");
+
+    c.fillStyle = gradient;
+
+    c.beginPath();
+    c.arc(this.pos.x, this.pos.y, this.flick ? Math.random() * this.size : this.size, 0, Math.PI * 2, true);
+    c.closePath();
+    c.fill();
+
+    c.restore();
+};
+
+Particle.prototype.exists = function() {
+    return this.alpha >= 0.1 && this.size >= 1;
+};
+
+function Rocket(x) {
+    Particle.apply(this, [{
+        x: x,
+        y: SCREEN_HEIGHT}]);
+
+    this.explosionColor = 0;
+}
+
+Rocket.prototype = new Particle();
+Rocket.prototype.constructor = Rocket;
+
+Rocket.prototype.explode = function() {
+    var count = Math.random() * 10 + 80;
+
+    for (var i = 0; i < count; i++) {
+        var particle = new Particle(this.pos);
+        var angle = Math.random() * Math.PI * 2;
+
+        // emulate 3D effect by using cosine and put more particles in the middle
+        var speed = Math.cos(Math.random() * Math.PI / 2) * 15;
+
+        particle.vel.x = Math.cos(angle) * speed;
+        particle.vel.y = Math.sin(angle) * speed;
+
+        particle.size = 10;
+
+        particle.gravity = 0.2;
+        particle.resistance = 0.92;
+        particle.shrink = Math.random() * 0.05 + 0.93;
+
+        particle.flick = true;
+        particle.color = this.explosionColor;
+
+        particles.push(particle);
+    }
+};
+
+Rocket.prototype.render = function(c) {
+    if (!this.exists()) {
+        return;
+    }
+
+    c.save();
+
+    c.globalCompositeOperation = 'lighter';
+
+    var x = this.pos.x,
+        y = this.pos.y,
+        r = this.size / 2;
+
+    var gradient = c.createRadialGradient(x, y, 0.1, x, y, r);
+    gradient.addColorStop(0.1, "rgba(255, 255, 255 ," + this.alpha + ")");
+    gradient.addColorStop(1, "rgba(0, 0, 0, " + this.alpha + ")");
+
+    c.fillStyle = gradient;
+
+    c.beginPath();
+    c.arc(this.pos.x, this.pos.y, this.flick ? Math.random() * this.size / 2 + this.size / 2 : this.size, 0, Math.PI * 2, true);
+    c.closePath();
+    c.fill();
+
+    c.restore();
+};
